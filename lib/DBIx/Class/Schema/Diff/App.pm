72e992f28ad550ab72f281b20decfb0ef5cc66e9
Jan Henning Thorsen <jhthorsen@cpan.org>
2010-07-08 16:45:37 +0200

    fix application class

--- lib/DBIx/Class/Schema/Diff/App.pm
+++ lib/DBIx/Class/Schema/Diff/App.pm
@@ -10,14 +10,29 @@ See L<DBIx::Class::Schema::Diff>
 
 =head1 SYNOPSIS
 
-    $ schema-diff.pl <from> <to>
+    $ schema-diff.pl \
+        --from <str> \
+        --to <str> \
+        --output path/to/dir \
+        ;
 
 =cut
 
+use DBIx::Class::Schema::Diff::Types qw/ Source /;
 use Moose;
 
 extends 'DBIx::Class::Schema::Diff';
 with qw/ MooseX::Getopt::Dashes /;
+
+MooseX::Getopt::OptionTypeMap->add_option_type_to_map(Source, '=s');
+
+has '+error' => ( traits => ['NoGetopt'] );
+has 'output' => (
+    is => 'ro',
+    isa => 'Str',
+    default => '.',
+    documentation => 'Where to output SQL to',
+);
 
 =head1 METHODS
 
@@ -29,7 +44,25 @@ Used to start the program from a perl script.
 
 sub run {
     my $self = shift;
-    my $args = $self->extra_argv;
+
+    unless($self->create_ddl_dir($self->output)) {
+        if($self->error) {
+            print "Unknown error!\n";
+            return 255;
+        }
+        else {
+            print $self->error, "\n";
+            return 1;
+        }
+    }
+
+    print "\nDiff was generated:\n";
+    printf " * From: %s\n", $self->to->filename($self->output);
+    printf " * To:   %s\n", $self->from->filename($self->output);
+    printf " * Diff: %s\n", $self->to->filename($self->output, $self->from->version);
+    print "\n";
+
+    return 0;
 }
 
 =head1 BUGS
